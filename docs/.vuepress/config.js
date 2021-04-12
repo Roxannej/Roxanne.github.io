484cb69016122a9f73223fdd991b220d83d8b1f6
Roxanne <1391753481@qq.com>
2021-04-12 21:18:04 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,56 +1,20 @@
 module.exports = {
-  title: '个人主页',
-  description: 'Personal Website',
-  head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/images/photo.jpg' }],
-    ['link', { rel: 'manifest', href: '/images/photo.jpg' }],
-    ['link', { rel: 'apple-touch-icon', href: '/images/photo.jpg' }],
-    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache'}],
-    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache,must-revalidate'}],
-    ['meta', { 'http-quiv': 'expires', cotent: '0'}]
-  ],
-  serviceWorker: true, // 是否开启 PWA
-  base: '/', // 部署到github相关的配置
-  markdown: {
-    lineNumbers: true // 代码块是否显示行号
-  },
+	// "deploy-gh": "GH=1 yarn docs:build && bash scripts/deploy-gh.sh"
+  base: '/blog-demo/',
+  title: 'blog-demo',
+  description: 'Vuepress blog demo',
   themeConfig: {
-    nav:[ // 导航栏配置
-      {text: '前端基础', link: '/accumulate/' },
-      {text: '算法题库', link: '/algorithm/'},
-      {text: '诗和远方', link: '/others/'},
-      {text: '微博', link: 'https://baidu.com'}      
-    ],
-    // sidebar:{
-    //   '/accumulate/': [
-    //       {
-    //         title: '前端积累',
-    //         children: [
-    //           '/accumulate/1.html',
-    //           '/accumulate/2.html',
-    //           '/accumulate/3.html',
-    //           '/accumulate/4.html',
-    //           '/accumulate/5.html',
-    //           '/accumulate/6.html',
-    //           '/accumulate/7.html',
-    //           '/accumulate/8.html',
-    //           '/accumulate/9.html',
-    //           '/accumulate/10.html',
-    //           '/accumulate/11.html',
-    //         ]
-    //       }
-    //     ],
-    //     '/algorithm/': [
-    //       '/algorithm/', 
-    //       {
-    //         title: '第二组侧边栏下拉框的标题1',
-    //         children: [
-    //           '/algorithm/' 
-    //         ]
-    //       }
-    //     ]
-    // },
-    sidebar: 'auto', // 侧边栏配置
-    sidebarDepth: 2
+	  	// 你的GitHub仓库，请正确填写
+	    repo: 'git@github.com:Roxannej/blog.git',
+	    // 自定义仓库链接文字。
+	    repoLabel: 'My GitHub',
+	  	nav: [
+	  		{ text: 'Home', link: '/' },
+	  		{ text: 'FirstBlog', link: '/blog/FirstBlog.md' }, 
+				{ text:'博客', 
+				items:[
+					{text:'vue', link:'/index/vue/vue/md'}
+				]}
+			]
   }
-};
+}
